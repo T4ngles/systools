65dd6ec299f646ec23510217b383d0ba0dbceb4c
T4ngles <rlau06@gmail.com>
2024-02-16 13:50:27 +1100

    sys_health - refactor, add in progress track, comparison to old log file, export log of new files

--- file_integrity/sys_health.py
+++ file_integrity/sys_health.py
@@ -6,9 +6,9 @@
         System Internal traffic monitor
         System External traffic monitor
         [X]File integrity (Start up and Shutdown scheduled check)
-            [ ]include progress bar of walk through files            
+            [X]include progress bar of walk through files            
             [ ]include comparison to malware hash database
-            [ ]read in most recent log for comparison*
+            [X]read in most recent log for comparison
             [ ]append to log with summary of file changes and additions
             [ ]create a sqlite database to serve the file hashes
             [ ]include Virus Total API for file hash inspection*
@@ -63,7 +63,8 @@ def main():
         vprint("")
 
     def print_space():
-        spacing = [print("") for x in range(2)]
+        spacing = [print("") for x in range(1)]
+        print(20*"-")
 
     #os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(sys.argv[0])))) #Documents folder #os.path.dirname(sys.argv[0]) #file_integrity folder
     #walk_dir = os.path.dirname(sys.argv[0]) #file_integrity folder
@@ -73,20 +74,7 @@ def main():
     print(hash_type + " File Integrity Check of " + walk_dir)
     startwalk_time = datetime.datetime.now()
 
-    #walk through files and compute hashes and add to dictionary of hashes
-    file_hash_dict_dict = {} 
-    for x,filepath in enumerate(file_walker.oswalk(walk_dir,verbose)):
-        #todo: add progress bar using enumerate somehow
-        file_hash = file_hasher.file_hash(filepath,hash_type) #calculate hash of file
-        filename = os.path.basename(filepath) #get filename
-        file_hash_dict = {"hash":file_hash,"filename":filename,"filepath":filepath,"hashfound":False} #create dictionary of file hash details 
-        file_hash_dict_dict[file_hash] = file_hash_dict
-
-    endwalk_time = datetime.datetime.now()
-
-    print_space()
-    print("file walk of " + str(len(file_hash_dict_dict)) + " files took " + str((endwalk_time-startwalk_time).total_seconds()) + " seconds")
-
+    #Load in latest log of hashes
     #get latest log file
     logs_path = os.path.dirname(sys.argv[0]) + "\\logs\\"
     file_type = r'\*csv'
@@ -98,57 +86,96 @@ def main():
         print("latest log found at " + latest_log)
     except ValueError:
         print("No Logs found. Creating log")
-        os.mkdir(logs_path)    
+        os.mkdir(logs_path)   #todo fix this need to actually create an empty log file? 
 
-    fieldnames = ["hash", "filename", "filepath", "hashfound"]
+    new_files = {} #create dictionary to store new file entries
 
     if latest_log != "":
+        
+        print('...loading latest hash log file')
         #open old hashlist csv and check current hashes with hashes in csv
         old_hash_dict = {}
-        new_files = {}
-
         with open(latest_log, 'r', encoding='utf-8', newline='') as readcsvfile:
             reader = csv.DictReader(readcsvfile)
             for item in reader:
                 old_hash_dict[item["hash"]] = item
 
-    #compare new hashlist to old and write out new hash summary log
+    oldLogSize = len(old_hash_dict)
+
+    #Main hash walk
+    #walk through files and compute hashes and add to dictionary of hashes
+    print('...hash walking the current files')
+    file_hash_dict_dict = {} 
+
+    for x,filepath in enumerate(file_walker.oswalk(walk_dir,verbose = False)):
+        #todo: add progress bar using enumerate somehow
+        progress = str(x) + (6-len(str(x)))*" " + f"/ {oldLogSize} files walked"
+        percStr = str(round(x/oldLogSize*100,0))
+        progress = (3-len(percStr))*" " + percStr + "%"
+        if not verbose:
+            print(progress, end="\r")
+        file_hash = file_hasher.file_hash(filepath,hash_type) #calculate hash of file
+        filename = os.path.basename(filepath) #get filename
+
+        #compare hash
+        if old_hash_dict:
+            if file_hash in old_hash_dict:
+                hashfound = True
+            else:
+                hashfound = False
+                new_files[file_hash] = file_hash_dict = {"hash":file_hash,"filename":filename,"filepath":filepath,"hashfound":hashfound}
+
+        file_hash_dict = {"hash":file_hash,"filename":filename,"filepath":filepath,"hashfound":hashfound} #create dictionary of file hash details 
+        file_hash_dict_dict[file_hash] = file_hash_dict
+        vprint(int(hashfound), " ", file_hash, ":", filename)
+
+    endwalk_time = datetime.datetime.now()
+
+    print_space()
+
+    
     log_name = logs_path + str(datetime.datetime.now().strftime("%d-%m-%Y %H%M%S"))+' - '+os.path.basename(walk_dir)+' - file_hash_dict.csv'     
     
     with open(log_name, 'w', encoding='utf-8', newline='') as csvfile:
-        
+        fieldnames = ["hash", "filename", "filepath", "hashfound"]
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
-        debug_block_heading("Hash table")
 
         for file_hash in file_hash_dict_dict.values():
-
-            if latest_log != "":
-                if file_hash["hash"] in old_hash_dict:
-                    file_hash["hashfound"] = True
-                else:
-                    file_hash["hashfound"] = False
-                    new_files[file_hash["hash"]] = file_hash
-
             writer.writerow(file_hash)
-            vprint(file_hash["hash"] + "    " + file_hash["filename"])
 
     endwrite_time = datetime.datetime.now()
+    print_space()
+    
+    print("log file saved at " + log_name)
 
     if new_files:
-        print("new files or modifications found for the following files:")
+        new_files_Number = str(len(new_files))
+        print( new_files_Number + " new files or modifications found:")
         for item in new_files.values():
-            print(item["filename"] + "    " + item["filepath"])
+            vprint(item["filename"] + "    " + item["filepath"])
+        #write out new hash file for inspection
+        new_files_log_name = logs_path + str(datetime.datetime.now().strftime("%d-%m-%Y %H%M%S"))+' - '+os.path.basename(walk_dir)+' - new_files.txt'
+        with open(new_files_log_name, 'w', encoding='utf-8', newline='') as csvfile:
+            fieldnames = ["filename", "filepath"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            for file_hash in new_files.values():
+                filtered_row = {key: value for key, value in file_hash.items() if key in fieldnames}
+                writer.writerow(filtered_row) 
+        print_space()
+        print("new files hash csv log saved at ", new_files_log_name)
+
     else:
         print("no new files or modifications found")
     
     print_space()
 
-    print("file walk of " + str(len(file_hash_dict_dict)) + " files took " + str((endwalk_time-startwalk_time).total_seconds()) + " seconds")
+    print(f"file walk of {str(len(file_hash_dict_dict))} files ({new_files_Number} new) took {str((endwalk_time-startwalk_time).total_seconds())} seconds")
     print("log comparison and write took " + str((endwrite_time-endwalk_time).total_seconds()) + " seconds")
-    print("log file saved at " + log_name)
 
     print_space()
+
 
     
 
