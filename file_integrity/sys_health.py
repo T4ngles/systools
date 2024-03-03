c47c2454f400be72e5b81fc30210e9203ef16bf2
T4ngles <rlau06@gmail.com>
2024-03-03 15:44:49 +1100

    sys_health - add duplicate files check, change dict key to file, path and name hash and refactor progress print

--- file_integrity/sys_health.py
+++ file_integrity/sys_health.py
@@ -97,50 +97,71 @@ def main():
         os.mkdir(logs_path)   #todo fix this need to actually create an empty log file? 
 
     new_files = {} #create dictionary to store new file entries
+    duplicate_files = {} #create dictionary to store duplicate file entries
 
     if latest_log != "":
         
         print('...loading latest hash log file')
         #open old hashlist csv and check current hashes with hashes in csv
-        old_hash_dict = {}
+        old_hash_set = set()
+        oldLogSize = 0
         with open(latest_log, 'r', encoding='utf-8', newline='') as readcsvfile:
             reader = csv.DictReader(readcsvfile)
             for item in reader:
-                old_hash_dict[item["hash"]] = item
-
-    oldLogSize = len(old_hash_dict)
+                file_key = item["hash"]+file_hasher.sha256_hasher_string(item["filepath"])+file_hasher.sha256_hasher_string(item["filename"])
+                old_hash_set.add(file_key)
+                oldLogSize = oldLogSize + 1
+     
+    print(f"{oldLogSize} files exist in old log")
 
     #Main hash walk
     #walk through files and compute hashes and add to dictionary of hashes
     print('...hash walking the current files')
     file_hash_dict_dict = {} 
+    walked_hashes_set = set()
 
     for x,filepath in enumerate(file_walker.oswalk(walk_dir,verbose = False, ignore_paths = PATHS_TO_IGNORE)):
         #todo: add progress bar using enumerate somehow
-        progress = str(x) + (6-len(str(x)))*" " + f"/ {oldLogSize} files walked"
-        percStr = str(round(x/oldLogSize*100,0))
+        percStr = str(round((x+1)/oldLogSize*100,0))
         progress = (3-len(percStr))*" " + percStr + "%"
-        if not verbose:
-            print(progress, end="\r")
-        file_hash = file_hasher.file_hash(filepath,hash_type) #calculate hash of file
+
+        file_hash = file_hasher.file_hash(filepath,hash_type) #calculate hash of file        
         filename = os.path.basename(filepath) #get filename
 
-        #compare hash
-        if old_hash_dict:
-            if file_hash in old_hash_dict:
+        filepath_hash = file_hasher.sha256_hasher_string(filepath) #calculate hash of filepath
+        filename_hash = file_hasher.sha256_hasher_string(filename) #calculate hash of filename
+
+        file_key = file_hash+filepath_hash+filename_hash
+        
+        #check if hash is already found in walk
+        try:
+            #hash found already in walk
+            if file_hash in walked_hashes_set:
+                vprint("===hash already found===")
+                duplicate_files[file_key] = {"filename":filename,"filepath":filepath}
+            #hash not found in walk yet
+        except KeyError:
+            walked_hashes_set.add(file_hash)    #add hash to hash set 
+
+        #compare hash to latest log hash todo change this to hashes set.
+        if old_hash_set:
+            if file_key in old_hash_set:  #hash found in previous log
                 hashfound = True
-            else:
+            else:   #hash isn't in previous log so is new file or has changed
                 hashfound = False
-                new_files[file_hash] = file_hash_dict = {"hash":file_hash,"filename":filename,"filepath":filepath,"hashfound":hashfound}
+                new_files[file_key] = {"filename":filename,"filepath":filepath}
 
-        file_hash_dict = {"hash":file_hash,"filename":filename,"filepath":filepath,"hashfound":hashfound} #create dictionary of file hash details 
-        file_hash_dict_dict[file_hash] = file_hash_dict
-        vprint(int(hashfound), " ", file_hash, ":", filename)
+        file_hash_dict = {"hash":file_hash,"filename":filename,"filepath":filepath,"hashfound":hashfound} #create dictionary of file hash details                         
+        file_hash_dict_dict[file_key] = file_hash_dict #add file hash dictionary to upper dictionary using walk_id and file hash as the key
 
+        vprint(int(hashfound), " ", file_hash, ":", len(file_hash_dict_dict), "/", filename, " at ", filepath)
+
+        if not verbose:
+            print(f"{progress} | files_walked:{(x+1)} new_files:{len(new_files)} duplicates:{len(duplicate_files)} | {file_hash} : {filename[0:60]}" + 8*"   ", end="\r")
+            
     endwalk_time = datetime.datetime.now()
 
     print_space()
-
     
     log_name = logs_path + str(datetime.datetime.now().strftime("%d-%m-%Y %H%M%S"))+' - '+os.path.basename(walk_dir)+' - file_hash_dict.csv'     
     
@@ -159,7 +180,7 @@ def main():
 
     if new_files:
         new_files_Number = str(len(new_files))
-        print( new_files_Number + " new files or modifications found:")
+        print( new_files_Number + " new files or modifications found")
         for item in new_files.values():
             vprint(item["filename"] + "    " + item["filepath"])
         #write out new hash file for inspection
@@ -173,9 +194,29 @@ def main():
                 writer.writerow(filtered_row) 
         print_space()
         print("new files hash csv log saved at ", new_files_log_name)
-
+    
     else:
         print("no new files or modifications found")
+    
+    if duplicate_files:
+        dup_files_Number = str(len(duplicate_files))
+        print( dup_files_Number + " duplicate files found")
+        for item in duplicate_files.values():
+            vprint(item["filename"] + "    " + item["filepath"])
+        #write out duplicate hash file for inspection
+        dup_files_log_name = logs_path + str(datetime.datetime.now().strftime("%d-%m-%Y %H%M%S"))+' - '+os.path.basename(walk_dir)+' - dup_files.txt'
+        with open(dup_files_log_name, 'w', encoding='utf-8', newline='') as csvfile:
+            fieldnames = ["filename", "filepath"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            for file_hash in duplicate_files.values():
+                filtered_row = {key: value for key, value in file_hash.items() if key in fieldnames}
+                writer.writerow(filtered_row) 
+        print_space()
+        print("dupilcate files hash csv log saved at ", dup_files_log_name)
+
+    else:
+        print("no duplicates found")
     
     print_space()
 
